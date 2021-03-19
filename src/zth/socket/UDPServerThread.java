1e59473c4cede1d08bc272f1d1df9d0fc7a036d3
zhangtianhao.0110 <zhangtianhao.0110@bytedance.com>
2021-03-19 18:25:15 +0800

    udp

---
+++ src/zth/socket/UDPServerThread.java
@@ -0,0 +1,59 @@
+package zth.socket;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UDPServerThread {
+    public static void main(String[] args) {
+        int port = 6789;
+        int threadCount = 0;
+        try{
+            DatagramSocket udpSocket = new DatagramSocket(port);
+            byte[] buffer = new byte[1024];
+            while(true){
+                DatagramPacket req = new DatagramPacket(buffer,buffer.length);
+                UDPThread udpThread = new UDPThread(udpSocket,req);
+                udpThread.run();
+                threadCount++;
+                System.out.println("The total number of clients is " + threadCount + ".");
+
+
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
+
+class UDPThread extends Thread {
+    DatagramSocket udpSocket = null;
+    DatagramPacket req = null;//接受的udp包
+
+    public UDPThread(DatagramSocket udpSocket, DatagramPacket req) {
+        this.udpSocket = udpSocket;
+        this.req = req;
+    }
+
+    public void run() {
+        try {
+            udpSocket.receive(req);
+            DatagramPacket reply = new DatagramPacket(req.getData(),
+                    req.getLength(), req.getAddress(),
+                    req.getPort());
+            udpSocket.send(reply);
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (udpSocket != null) udpSocket.close();
+        }
+
+
+    }
+}
